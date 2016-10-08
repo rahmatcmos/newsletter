d558c43198a0292da16c6da516ce777b09f6c8d9
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-10-08 09:16:18 +0700

    Show README.content as default dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $about = \Markdown::convertToHtml(\File::get('../README.md'));
+        return view('home', compact('about'));
     }
 
     public function getAbout()
