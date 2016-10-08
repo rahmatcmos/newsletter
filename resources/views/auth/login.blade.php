29768f675d7990500701474b85606b1382188349
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-10-08 16:55:23 +0700

    Bootstrap validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,13 +7,13 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form role="form" method="POST" action="{{ url('/login') }}">
+                    <form role="form" method="POST" action="{{ url('/login') }}" data-toggle="validator">
                         {{ csrf_field() }}
                         {{ method_field('post') }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="control-label">E-Mail Address</label>
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
                             @if ($errors->has('email'))
                                 <span class="help-block">
                                     <strong>{{ $errors->first('email') }}</strong>
@@ -23,7 +23,7 @@
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="control-label">Password</label>
-                            <input id="password" type="password" class="form-control" name="password">
+                            <input id="password" type="password" class="form-control" name="password" required>
                             @if ($errors->has('password'))
                                 <span class="help-block">
                                     <strong>{{ $errors->first('password') }}</strong>
@@ -55,3 +55,7 @@
     </div>
 </div>
 @endsection
+
+@push('js')
+    <script src="{{ asset('component/bootstrap-validator/dist/validator.min.js') }}"></script>
+@endpush
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <form role="form" method="POST" action="{{ url('/register') }}">
+                    <form role="form" method="POST" action="{{ url('/register') }}" data-toggle="validator">
                         {{ csrf_field() }}
                         {{ method_field('post') }}
 
@@ -71,3 +71,7 @@
     </div>
 </div>
 @endsection
+
+@push('js')
+    <script src="{{ asset('component/bootstrap-validator/dist/validator.min.js') }}"></script>
+@endpush
