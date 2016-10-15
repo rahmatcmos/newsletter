cf5d72686c519f80cd3dc2e5e87d9e6b3a155086
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-10-15 13:11:38 +0700

    Change model class

--- database/seeds/SubscriberTableSeeder.php
+++ database/seeds/SubscriberTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\NewsletterList;
-use App\Subscriber;
+use App\NewsletterSubscriber;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -17,16 +17,16 @@ class SubscriberTableSeeder extends Seeder
         // find default list
         $list = NewsletterList::whereIsDefault(true)->first();
 
-        $subscriber = new Subscriber();
+        $subscriber = new NewsletterSubscriber();
         $faker = Faker\Factory::create('id_ID');
         foreach (range(1, 100) as $index) {
             DB::table($subscriber->getTable())->insert([
                 'newsletter_list_id' => $list->id,
-                'name'               => $faker->name,
-                'email'              => $faker->email,
-                'status'             => 'subscribed',
-                'created_at'         => Carbon::now(),
-                'updated_at'         => Carbon::now(),
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'status' => 'subscribed',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ]);
         }
     }
