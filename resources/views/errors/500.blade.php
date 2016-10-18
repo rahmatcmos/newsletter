fb57e051bda6cf033960efb560ec15631e0341db
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-10-18 09:04:53 +0700

    Allow html tag on error message

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="panel-body text-center">
                 	<h1>{{ $exception->getStatusCode() }}</h1>
-                    <h4>{{ $exception->getMessage() }}</h4>
+                    <h4>{!! $exception->getMessage() !!}</h4>
                 	<p>
                 		<a href="{{ route('home') }}" class="btn btn-primary">Back to Home</a>
                         <a href="{{ url()->previous() }}" class="btn btn-default">Back to Previous Page</a>
